2b46c099407019853886bd8b0115927e516d9fcf
vadym vasylaki <vadym@vadyms-MacBook-Pro.local>
2025-10-25 07:54:18 +0300

    init commit

---
+++ QrCode/QrCode/QrCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodeApp.swift
+//  QrCode
+//
+//  Created by vadym vasylaki on 25.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct QrCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
